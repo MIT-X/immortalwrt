83132380f9b368db62323f64031337003a641212
MIT-X <134132915+MIT-X@users.noreply.github.com>
2024-12-23 13:19:39 +0800

    Add files via upload

---
+++ DIY-1.sh
@@ -0,0 +1,7 @@
+# 此脚本用处是：添加第三方插件
+#=================================================================================
+
+# 1-添加 ShadowSocksR Plus+ 插件
+# echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+
+
---
+++ DIY-2.sh
@@ -0,0 +1,4 @@
+rm -rf ./feeds/luci/applications/luci-app-passwall
+rm -rf ./feeds/luci/applications/luci-app-openclash
+
+sed -i 's/KERNEL_PATCHVER:=5.15/KERNEL_PATCHVER:=6.12/g' ./target/linux/x86/Makefile
