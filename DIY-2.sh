5f27c36a2df28e3ff02c06692e2b966434b31553
MIT-X <134132915+MIT-X@users.noreply.github.com>
2024-12-24 12:59:20 +0800

    Update DIY-2.sh

--- DIY-2.sh
+++ DIY-2.sh
@@ -1,4 +1,3 @@
 rm -rf ./feeds/luci/applications/luci-app-passwall
 rm -rf ./feeds/luci/applications/luci-app-openclash
-
 # sed -i 's/KERNEL_PATCHVER:=5.15/KERNEL_PATCHVER:=6.12/g' ./target/linux/x86/Makefile
