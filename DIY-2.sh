11485d1d1d2e054044079859d595d47310f60fe6
MIT-X <134132915+MIT-X@users.noreply.github.com>
2024-12-26 11:01:17 +0800

    Update DIY-2.sh

--- DIY-2.sh
+++ DIY-2.sh
@@ -8,6 +8,6 @@ rm -rf ./feeds/luci/applications/luci-app-openclash && cp -rf ./feeds/openclash 
 # 1-更改默认地址
 # sed -i 's/192.168.1.1/192.168.2.222/g' package/base-files/files/bin/config_generate
 # 2-设置密码为空
-sed -i '/CYXluq4wUazHjmCDBCqXF/d' package/lean/default-settings/files/zzz-default-settings
+# sed -i '/CYXluq4wUazHjmCDBCqXF/d' package/lean/default-settings/files/zzz-default-settings
 # 3-修改内核版本
 sed -i 's/KERNEL_PATCHVER:=6.6/KERNEL_PATCHVER:=6.12/g' ./target/linux/x86/Makefile
