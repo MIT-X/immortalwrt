5453c3d0ab3137eb137c60aa2540a734d8fc19dc
MIT-X <134132915+MIT-X@users.noreply.github.com>
2024-12-26 10:40:02 +0800

    Update DIY-2.sh

--- DIY-2.sh
+++ DIY-2.sh
@@ -1,3 +1,16 @@
-rm -rf ./feeds/luci/applications/luci-app-passwall
-rm -rf ./feeds/luci/applications/luci-app-openclash
-# sed -i 's/KERNEL_PATCHVER:=5.15/KERNEL_PATCHVER:=6.12/g' ./target/linux/x86/Makefile
+# 此脚本用处是：定制个性化参数
+#=====================================================================
+rm -rf feeds/packages/lang/golang
+git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
+rm -rf ./feeds/luci/applications/luci-app-passwall && cp -rf ./feeds/passwall ./feeds/luci/applications/luci-app-passwall
+rm -rf ./feeds/luci/applications/luci-app-passwall2 && cp -rf ./feeds/passwall2 ./feeds/luci/applications/luci-app-passwall2
+rm -rf ./feeds/luci/applications/luci-app-openclash && cp -rf ./feeds/openclash ./feeds/luci/applications/luci-app-openclash
+
+# 1-更改默认地址
+# sed -i 's/192.168.1.1/192.168.2.222/g' package/base-files/files/bin/config_generate
+
+# 2-设置密码为空
+sed -i '/CYXluq4wUazHjmCDBCqXF/d' package/lean/default-settings/files/zzz-default-settings
+
+# 3-修改内核版本
+sed -i 's/KERNEL_PATCHVER:=6.6/KERNEL_PATCHVER:=6.12/g' ./target/linux/x86/Makefile
