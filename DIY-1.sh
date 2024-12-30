cdccff47975fa83217a6bc504cc843710193ea76
MIT-X <134132915+MIT-X@users.noreply.github.com>
2024-12-30 21:52:46 +0800

    Update DIY-1.sh

--- DIY-1.sh
+++ DIY-1.sh
@@ -2,7 +2,5 @@
 #=======================================================================================================================
 #sed -i '$a\src-git openclash https://github.com/vernesong/OpenClash' ./feeds.conf.default
 #echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main" >> "feeds.conf.default"
-
-
 echo "src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git;main" >> "feeds.conf.default"
 echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
