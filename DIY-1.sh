3f580494e5591e4ee93580ad2e52056b8e417bb0
MIT-X <134132915+MIT-X@users.noreply.github.com>
2024-12-30 21:52:05 +0800

    Update DIY-1.sh

--- DIY-1.sh
+++ DIY-1.sh
@@ -1,6 +1,8 @@
 # 此脚本用处是：添加第三方插件
 #=======================================================================================================================
-sed -i '$a\src-git openclash https://github.com/vernesong/OpenClash' ./feeds.conf.default
-echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main" >> "feeds.conf.default"
+#sed -i '$a\src-git openclash https://github.com/vernesong/OpenClash' ./feeds.conf.default
+#echo "src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main" >> "feeds.conf.default"
+
+
 echo "src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git;main" >> "feeds.conf.default"
 echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
